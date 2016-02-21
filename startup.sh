a7813c30d9157842f7dff22a91a44e03799d2440
Kev <spam4kev@gmail.com>
2016-02-21 07:07:37 -0800

    wip

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 if [ $(curl -s http://169.254.169.254/latest/meta-data/hostname/) == mesos-1.* ];then
-    /root/mesos-*/bin/mesos-master.sh --ip=$eth0_ip --work_dir=/var/lib/mesos
+    /root/mesos-0.27.0/bin/mesos-master.sh --ip=$eth0_ip --work_dir=/var/lib/mesos
 fi
-/root/mesos-*/bin/mesos-slave.sh --master=mesos-master:5050
+/root/mesos-0.27.0/bin/mesos-slave.sh --master=mesos-master:5050
